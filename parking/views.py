5898d31b1620b22e1e3f47c3a13e095650c34216
SergioSoteras <sergiosoteras@hotmail.com>
2021-11-17 01:10:07 +0100

    contacto

--- parking/views.py
+++ parking/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def contacto(request):
+    '''
+    Pagina de contacto de nuestra web
+    '''
+    datos = {'autor':'Sergio Soteras',
+            'email': 'emaildecontacto@gmail.com',
+            'fax': '976542198'}
+    
+    return render(request, 'contacto.html', context=datos)
