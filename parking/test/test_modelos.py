73bd9163c4240f78821b82d16b02575cd23becb5
SergioSoteras <sergiosoteras@hotmail.com>
2022-05-18 18:44:19 +0200

    test_modelos

--- parking/test/test_modelos.py
+++ parking/test/test_modelos.py
@@ -18,6 +18,10 @@ class ClienteTestCase(TestCase):
        self.assertEqual(cliente.__str__(),'Sergio Soteras Serrano')
        self.assertEqual(cliente.vehiculo,'C4')
 
+    def test_cliente_plaza(self):
+        cliente = Cliente.objects.get(dni='73012345H')
+        self.assertEqual(cliente.plaza,Plaza.objects.get(numero=1))
+
 #Cliente modificado correctamente
     def test_modificar_cliente(self):
         cliente = Cliente.objects.get(dni='73012345H')
@@ -45,8 +49,15 @@ class PlazaTestCase(TestCase):
 
     def test_plaza_creada_correctamente(self):
        plaza1= Plaza.objects.get(numero=1)
-       self.assertEquals(plaza1.planta,1)
+       self.assertEqual(plaza1.planta,1)
 
+    def test_plaza_dimensiones(self):
+        plaza1=Plaza.objects.get(numero=1)
+        self.assertEqual(plaza1.dimensiones,Dimensiones.objects.get(tipo='prueba'))
+
+    def test_str_plaza(self):
+        plaza1=Plaza.objects.get(numero=1)
+        self.assertEqual(plaza1.__str__(),'Plaza 1 ( Planta 1 )')
 #Plaza disponible
     def test_plaza_disponible(self):
         plaza1= Plaza.objects.get(numero=1)
@@ -55,3 +66,28 @@ class PlazaTestCase(TestCase):
         cliente.plaza = plaza1
         cliente.save()
         self.assertFalse(plaza1.disponible())
+
+#Cliente de la plaza
+    def test_plaza_cliente(self):
+        plaza1= Plaza.objects.get(numero=1)
+        cliente = Cliente.objects.get(dni='73012345H')
+        cliente.plaza = plaza1
+        cliente.save()
+        self.assertEqual(plaza1.nombre_cliente(),cliente)
+
+#Dimensiones creada correctamente
+class DimensionesTestCase(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Dimensiones.objects.create(tipo='prueba',altura=2,anchura=2,largura=2)
+
+    def test_dimensiones_creada(self):
+        dim = Dimensiones.objects.get(tipo='prueba')
+        self.assertEqual(dim.altura,2)
+        self.assertEqual(dim.anchura,2)
+        self.assertEqual(dim.largura,2)
+    
+    def test_str_dimensiones(self):
+        dim = Dimensiones.objects.get(tipo='prueba')
+        self.assertEqual(dim.__str__(),'prueba (2.00m x 2.00m x 2m)')
